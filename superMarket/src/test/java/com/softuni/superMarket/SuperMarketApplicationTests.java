55b1e7b6d6215eb084a83a150be99cdc463e8b14
Danil <danilogomanjuk93@gmail.com>
2024-10-06 12:41:39 +0200

    first Commit

---
+++ superMarket/src/test/java/com/softuni/superMarket/SuperMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softuni.superMarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
