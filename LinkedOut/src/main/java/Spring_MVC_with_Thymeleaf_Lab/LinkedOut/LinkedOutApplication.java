8851b4b2fb3446636abe02464803ae39ea51cd0e
Danil <danilogomanjuk93@gmail.com>
2024-10-19 18:31:45 +0200

    new practical...

---
+++ LinkedOut/src/main/java/Spring_MVC_with_Thymeleaf_Lab/LinkedOut/LinkedOutApplication.java
@@ -0,0 +1,13 @@
+package Spring_MVC_with_Thymeleaf_Lab.LinkedOut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkedOutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinkedOutApplication.class, args);
+	}
+
+}
---
+++ LinkedOut/src/test/java/Spring_MVC_with_Thymeleaf_Lab/LinkedOut/LinkedOutApplicationTests.java
@@ -0,0 +1,13 @@
+package Spring_MVC_with_Thymeleaf_Lab.LinkedOut;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkedOutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
