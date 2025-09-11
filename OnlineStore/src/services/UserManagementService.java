c489c517bfe53b198242d5b90f0b315271f08b6a
Danilo Gomanjuk <danilogomanjuk93@gmail.com>
2025-09-11 18:35:44 +0200

    Add exam templates

---
+++ OnlineStore/src/services/UserManagementService.java
@@ -0,0 +1,14 @@
+package services;
+
+
+import enteties.User;
+
+public interface UserManagementService {
+
+	String registerUser(User user);
+	
+	User[] getUsers();
+
+	User getUserByEmail(String userEmail);
+
+}
