b069fdb61cc7be9f047eb4cdc3d3100e3818e15d
Danilo Gomanjuk <danilogomanjuk93@gmail.com>
2025-10-02 18:45:02 +0200

    Udemy Course exercise - Implement Author class

---
+++ BookExersiceOneStream_API/src/Model/Author.java
@@ -0,0 +1,41 @@
+package Model;
+
+public class Author {
+    private int id;
+    private String firstName;
+    private String lastName;
+
+    public Author(int id, String firstName, String lastName) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public Author() {
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "Author{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+}
